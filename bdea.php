85a55631daf0afc7469f80114e40a86702df61a3
gsetz <gsetz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 20:52:05 +0000

    Version 0.6, updated plugin to use easyapi / json
    git-svn-id: https://plugins.svn.wordpress.org/block-disposable-email-addresses/tags/0.6@1142576 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bdea.php
@@ -0,0 +1,177 @@
+<?php
+/*
+	Plugin Name: Block Disposable EMail 
+	Plugin URI: http://wordpress.org/extend/plugins/block-disposable-email-addresses/ 
+	Description: This plugin protects your registered user base by preventing registration with a disposable email addresse (like mailinator). 
+	Author: Gerold Setz 
+	Version: 0.6
+	Author URI: http://www.block-disposable-email.com/
+	Text Domain: bdea 
+	Domain Path: /bdea
+ */
+
+// Check if we are running PHP5 or above, otherwise die with an error message.
+function bdea_activation_check()
+{
+        if (version_compare(PHP_VERSION, '5.0.0', '<'))
+        {
+                deactivate_plugins(basename(__FILE__));
+                wp_die("Sorry, but you can't run this plugin, it requires PHP 5 or higher.");
+        }
+}
+register_activation_hook(__FILE__, 'bdea_activation_check');
+
+add_action('admin_menu', 'block_dea_menu');
+
+function block_dea_menu() {
+        add_options_page('Block DEA Options', 'Block DEA', 'manage_options', 'bdea-admin-menu', 'block_dea_options');
+        add_action( 'admin_init', 'register_bdea_settings' );
+}
+
+function plugin_section_text() {
+        echo '<p>This plugin requires an api key. You can get one at <a href="http://www.block-disposable-email.com/cms/register/" target="_bdea">www.block-disposable-email.com</a>.</p>';
+	echo '<p>When subscribing for an api key you will also be asked for your servers ip address: it seems to be <i>'.$_SERVER['SERVER_ADDR'].'</i>.</p>';
+	}
+
+function plugin_section_status() {
+	/// Status abfragen
+        $key = get_option('bdea_plugin_options');
+        $request = 'http://status.block-disposable-email.com/status/?apikey='.$key['bdea_api_key'];
+
+	if (!$key['bdea_api_key'])
+		{
+		echo 'No API key entered so far. Please get one and insert it here.';
+		}
+	else
+		{
+        	if ($response = @file_get_contents($request))
+			{
+        		$status = json_decode($response);
+//print_r($status);
+        		if ($status->request_status == 'ok' && $status->apikeystatus== 'active') 
+				{
+				echo 'Everything fine! The API key you entered is valid. <p>Currently (as of '.$status->credits_time.') there are '.$status->credits.' credits. Especially when you use a pretty new generated api key the number might be wrong. Simply come back in about 30 minutes and check again.</p>';
+				if ($status->credits <=0) echo '<p><b>Warning:</b> All your credits are used up so far! The service will now always respond with an OK message that means that disposable email addresses are not recognized until you add credits. Free credits will be added on the 1st of every month. Please consider to buy commercial credits. For further information see <a href="http://www.block-disposable-email.com/pricing.php" target="_bdea">block-disposable-email.com</a></p>';
+				}
+			else
+				{
+				echo '<div id="message" class="error">Something is wrong with your api key. The server responded with '.$status->apikeystatus.'.</div>';
+				}
+			}
+		else echo 'No response from server. Please try later.';
+		}
+}
+
+function plugin_setting_string() {
+        $options = get_option('bdea_plugin_options');
+        echo "<input id='plugin_text_string' name='bdea_plugin_options[bdea_api_key]' size='40' type='text' value='{$options['bdea_api_key']}' />";
+}
+
+
+function register_bdea_settings() {
+        register_setting( 'bdea_plugin_options', 'bdea_plugin_options');
+        add_settings_section('plugin_status', 'Status of your API key', 'plugin_section_status', 'plugin');
+        add_settings_section('plugin_main', 'Main Settings', 'plugin_section_text', 'plugin');
+        add_settings_field('plugin_text_string', 'BDEA Api key', 'plugin_setting_string', 'plugin', 'plugin_main');
+}
+
+ // Add a settings link in the plugin listing
+	add_filter('plugin_action_links', 'plugin_action_links', 10, 2);
+
+function block_dea_options() {
+?>
+<div>
+<h2>Block Disposable Email</h2>
+Options relating to the BDEA plugin.
+<form action="options.php" method="post">
+<?php settings_fields('bdea_plugin_options'); ?>
+<?php do_settings_sections('plugin'); ?>
+
+<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+</form></div>
+
+<?php
+}
+
+function plugin_action_links($links, $file)
+        {
+                static $this_plugin;
+                if (!$this_plugin)
+                {
+                        $this_plugin = plugin_basename(__FILE__);
+                }
+
+                if ($file == $this_plugin)
+                {
+                        $settings_link = '<a href="options-general.php?page=bdea-admin-menu">' . __('Settings') . '</a>';
+                        array_unshift($links, $settings_link);
+                }
+
+                return $links;
+        }
+
+// Hooks, Funktionen
+add_filter ( 'is_email', 'bdea_check' );
+
+function bdea_check ($email){
+        //ini_set('default_socket_timeout',5);
+        list(, $domain) = explode('@', $email);
+
+        $key = get_option('bdea_plugin_options');
+        $request = 'http://check.block-disposable-email.com/easyapi/json/'.$key['bdea_api_key'].'/'.trim($domain);
+
+	//echo $request;
+
+        if ($response = @file_get_contents($request))
+		{
+        	$dea = json_decode($response);
+
+        	if ($dea->request_status == 'success')
+                	{
+                	if ($dea->domain_status == 'ok')
+                        	{
+                        	// do something like register ...
+                        	return true;
+                        	}
+
+                	if ($dea->domain_status == 'block')
+                        	{
+                        	// deny registration ...
+                        	return false;
+                        	}
+                	}
+       		else return true;
+		}
+	else return true;
+}
+
+// Message after activation
+
+/**
+ * Generic function to show a message to the user using WP's
+ * standard CSS classes to make use of the already-defined
+ * message colour scheme.
+ *
+ * @param $message The message you want to tell the user.
+ * @param $errormsg If true, the message is an error, so use
+ * the red message style. If false, the message is a status
+  * message, so use the yellow information message style.
+ */
+function showActivationMessage($message, $errormsg = true)
+{
+	if ($errormsg) {
+		echo '<div id="message" class="error">';
+	}
+	else {
+		echo '<div id="message" class="updated fade">';
+	}
+	
+	$message = '<h3>Warning from the Block Disposable E-Mail Plugin </h3><p>Please insert a valid api key!</p><p>The plugin will not work correctly otherwise ...</p><p>You can get one at <a href="http://www.block-disposable-email.com/cms/register/" target="_bdea">www.block-disposable-email.com</a>.</p>';
+	echo "<p><strong>$message</strong></p></div>";
+}    
+
+$key = get_option('bdea_plugin_options');
+if (!$key['bdea_api_key']) add_action('admin_notices', 'showActivationMessage');
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,86 @@
+=== Block Disposable Email ===
+Contributors: gsetz 
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZVJSLJALXCKYQ 
+Tags: comments, posts, spam, disposable email, temporary email, validation, anti-spam, spam, mailinator, authentication, disposable, email, guerrillamail,mailinator, register, registration, fraud 
+Requires at least: 3.3.1
+Tested up to: 3.3.1
+Stable tag: trunk
+
+This plugin detects one-time email addresses (disposable email, trashmail, mailinator, 10minutemail) and helps to keep your userbase and comments clean. Free registration at www.block-disposable-email.com needed.
+
+== Description ==
+
+This plugin prevents people from registering with disposable email addresses (dea) like the ones provided by mailinator (also known as throw-away email, one-time email). It protects your most important asset, your registered user base, by preventing contamination by fake accounts. This plugin working principle is similar to spam blacklists.
+
+It hooks in the wordpress function is_email() so it will extend the known email validation of wordpress to detect dea domains. 
+
+The plugin itself does not contain a list of domains to block. Instead of local maintenance the plugin uses the service of http://www.block-disposable-email.com. This is a very accurate free service for up to 200 queries a month. For huge sites several commercial plans are available.
+
+Please see the FAQ section for some more information.
+
+== Installation ==
+
+1. Unpack the *.zip file and extract the /block-disposable-email-addresses/ folder and the files.
+2. Using an FTP program, upload the full /block-disposable-email-addresses/ folder to your WordPress plugins directory (Example: /wp-content/plugins).
+3. Register at www.block-disposable-email.com and pick your api key. Please note that you have to register your server with its ip address to get a key. Simply follow the guideline of the service.
+4. Go to Plugins, click Settings and insert your api key.
+5. Go to Plugins and activate the plugin.
+
+== Frequently Asked Questions ==
+
+= What about privacy? =
+
+This plugin does NOT submit email addresses. I do not run this service to collect your subscribers data. 
+
+Before the data is sent to http://www.block-disposable-email.com the domain part is separated. So only the domain part is sent. For further information see the mentioned website.  
+
+= What happens if the service is down? =
+
+First of all I try to have minimum downtime. That's why there are currently 3 servers at different locations (UK, US, AT) waiting for your requests. For further information see http://www.block-disposable-email.com/faq.php
+
+And yes, even if the service is down your users are able to leave comments.
+
+= What happens if my credits are used up? =
+
+If your credits are running low you will be informed by email. If the credits are used up you get an additional email. In case you do not have any credits left the service will always respond with an OK (like the used domain is valid to subscribe). 
+
+In other words: even if you do not have any credits left your subscribers are allowed to subscribe (or leave comments or whatever). But users subscribing with trashmail / disposable email addresses are also allowed ...
+
+Please see http://www.block-disposable-email.com/pricing.php if you need more than the 200 free credits a month. 
+
+= Why should I use this service? I use CAPTCHA so it is hard for robots to register automatically. Captcha normally does a great job. For robots it is hard to register indeed. =
+
+What Captcha cannot block are human subscribers using one-time email-addresses. It is bad to have such addresses in your userbase for several reasons. And yes, it is easy to block *.ru addresses, known domains as trashmail.net, and some others - but they are changing continuously. This service currently detects about 3.000 domains. So maybe it is a good alternative for your manually maintained blocklist.
+
+= I got a "BLOCK" response for a domain which is running no dea (disposable email address) services. How can I prevent or correct these false positives? =
+
+This should be happen very rarely. In this case I kindly ask you to report this mistake. 
+
+= Is there a documentation for the api in case I need to use the service without Wordpress? =
+
+Sure. Have a look at http://www.block-disposable-email.com/usage.php. There are several documents for the json, txt and php-serialized api.
+
+== Changelog ==
+
+= 0.5 =
+* Added support for Wordpress 4.1.2
+* Changed used api to easyapi (see http://www.block-disposable-email.com/cms/help-and-usage/easy-api/)
+
+= 0.5 =
+* Added a huge warning message in case no api key has been inserted after activation. Should make clear that the plugin does not work without the free registration.
+
+= 0.4 =
+* Changed host for status messages from www. to status.block-disposable-email.com
+* In the admin section of the plugin the server ip will be listed to make it easier to register an api key.
+* Added some more information to readme.txt regarding privacy and other issues. 
+* Added a FAQ section.
+
+= 0.3 =
+* Added status request for the admin part of the plugin. Shows now if a entered api key is valid or not. Additionally the number of free credits are shown. 
+
+= 0.2 =
+* First version released.
+
+== Upgrade Notice ==
+
+Simply reinstall.
